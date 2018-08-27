42e378211d8acfc92f1e5cf7223120e3877ab178
Lucas Smolic <lucas.smolic@gmail.com>
2018-08-27 16:00:27 -0700

    added the proper gem definition for master branch and updated the service to work on docker. probably need to update this to be more generic

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'https://rubygems.org'
 
 ruby '2.3.5'
 
-gem 'snip', git: "https://github.com/lsmolic/snip-grpc-gem", tag: 'master'
+gem 'snip', git: "https://github.com/lsmolic/snip-grpc-gem", branch: 'master'
+# gem 'snip', path: '../snip'
 gem 'grpc', '~> 1.0'
--- lib/services/snip_service.rb
+++ lib/services/snip_service.rb
@@ -1,10 +1,16 @@
 require 'grpc'
 require 'snip_services_pb'
+require 'bop_services_pb'
 
 class SnipService < Snip::UrlSnipService::Service
 	def snip_it(snip_req, _unused_call)
 		puts "Received URL snip request for #{snip_req.url}"
-		Snip::SnipResponse.new(url: snip_req.url)
+		stub = Bop::BopService::Stub.new('bop:50053', :this_channel_is_insecure)
+		req = Bop::BopRequest.new(url: snip_req.url)
+		resp_obj = stub.bop_it(req)
+
+		Snip::SnipResponse.new(url: resp_obj.url)
+		# Snip::SnipResponse.new(url: snip_req.url)
 	end
 end
 
